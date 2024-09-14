cc00b0ba3098702b7c05afd31f1d304d7fe1f340
leejung7308 <leekkzzzz@naver.com>
2024-09-15 04:40:25 +0900

    함수 구현

---
+++ UnrealStudy/Source/UnrealStudy/MyActor.cpp
@@ -0,0 +1,48 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyActor.h"
+
+// Sets default values
+AMyActor::AMyActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	TotalDamage = 400;
+	DamageTimeInSeconds = 2.5f;
+}
+
+// Called when the game starts or when spawned
+void AMyActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMyActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AMyActor::PostInitProperties()
+{
+	Super::PostInitProperties();
+
+	CalculateDPS();
+}
+
+void AMyActor::PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent)
+{
+	CalculateDPS();
+	
+	Super::PostEditChangeProperty(PropertyChangedEvent);
+}
+
+void AMyActor::CalculateDPS()
+{
+	DamagePerSecond = TotalDamage / DamageTimeInSeconds;
+}
+
